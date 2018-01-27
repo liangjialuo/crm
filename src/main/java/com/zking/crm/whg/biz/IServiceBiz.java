a76cd97e3d54336a200ae34971107fa177174d0d
01 <718961208>
2018-01-27 21:09:22 +0800

    1111

---
+++ src/main/java/com/zking/crm/whg/biz/IServiceBiz.java
@@ -0,0 +1,12 @@
+package com.zking.crm.whg.biz;
+
+import com.zking.crm.base.model.Service;
+
+public interface IServiceBiz {
+    void add(Service service);
+
+    void edit(Service service);
+
+    void del(Service service);
+
+}
